4f27c16e5f612d22f35d1ff372fdeedde08c571e
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-12-14 21:29:01 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Wojuade Abdul Afeez on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
